d33adaa2e61afcbc4b1a996eb0117e19a4556770
web3muhammad <web3muhammad@icloud.com>
2025-08-03 19:14:35 +0300

    fix: init data import

--- src/services/login/index.ts
+++ src/services/login/index.ts
@@ -1,9 +1,12 @@
+import { api } from "../api";
+
 export async function getAccessToken() {
   const webAppInitData = Telegram.WebApp.initData;
   const res = await api.post("/auth/telegram", {
-    init_data: webAppInitData, // Используем реальные данные вместо хардкода
+    init_data: webAppInitData,
   });
 
   localStorage.setItem("access_token", res.data?.access_token);
+
   return res;
 }
